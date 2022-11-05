8003109048110b52c20fe11e8af77e7a1fa5be9b
shounak77 <51368579+shounak77@users.noreply.github.com>
2022-11-05 16:57:27 +0100

    update code to take cmd args

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -6,12 +6,12 @@ void display(char *values){
     char array[32];
     std:: strcpy(array, values);
     std:: cout << values << "\n";
-    std:: cout << "Size: " << sizeof(array) << "\n";
+    std:: cout << "Size: " << sizeof(values) << "\n";
 }
 
 int main(int argc, char** argv) {
-    std::cout << "Hello World!\n";
-    std::cout << "Argument count: " << argc - 1 << "\nValue(s): \n";
+    std::cout << "Hello Brave New World!\n";
+    std::cout << "Args #: " << argc - 1 << "\nVal(s): \n";
     for(int i = 1; i < argc; ++i){
         display(argv[i]);
     }
