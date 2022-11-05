b760fb38d065173a5393c5f3e71ba474f1b11f71
shounak77 <51368579+shounak77@users.noreply.github.com>
2022-11-05 16:57:27 +0100

    print results to std out

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
-#include<cstring>
+#include <cstring>
 
 
-void display(char *values){
+void print_vals_to_stdout(char *values){
     char array[32];
     std:: strcpy(array, values);
     std:: cout << values << "\n";
-    std:: cout << "Size: " << sizeof(values) << "\n";
+    std:: cout << "Size: " << sizeof(array) << "\n";
 }
 
 int main(int argc, char** argv) {
     std::cout << "Hello Brave New World!\n";
     std::cout << "Args #: " << argc - 1 << "\nVal(s): \n";
     for(int i = 1; i < argc; ++i){
-        display(argv[i]);
+        print_vals_to_stdout(argv[i]);
     }
     return 0;
 }
