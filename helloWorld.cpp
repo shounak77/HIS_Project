47139f844c22bf20a78551d20f88ddc0105e7734
shounak77 <51368579+shounak77@users.noreply.github.com>
2022-11-05 16:57:27 +0100

    Update helloWorld.cpp

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include<cstring>
+
+
+void display(char *values){
+    char array[32];
+    std:: strcpy(array, values);
+    std:: cout << values << "\n";
+    std:: cout << "Size: " << sizeof(array) << "\n";
+}
 
 int main(int argc, char** argv) {
     std::cout << "Hello World!\n";
-    return 0;
+    std::cout << "Argument count: " << argc - 1 << "\nValue(s): \n";
+    for(int i = 1; i < argc; ++i){
+        display(argv[i]);
     }
+    return 0;
+}
